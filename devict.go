0125157e260a0e97c1df1c3299c8d7c81112a3ea
Aaron Alexander <aarona.arduino@gmail.com>
2016-06-29 16:34:49 -0500

    done some work

--- devict.go
+++ devict.go
@@ -6,84 +6,124 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"reflect"
 )
 
-type v struct {
-	country                string
-	localized_country_name string
-	city                   string
-	address_1              string
-	name                   string
-	lat                    float64
-	lon                    float64
-	id                     int
-	state                  string
-	repinned               bool
+type ResultsTwo struct {
+	UtcOffset int `json:"utc_offset"`
+	Venue     struct {
+		Zip                  string  `json:"zip"`
+		Country              string  `json:"country"`
+		LocalizedCountryName string  `json:"localized_country_name"`
+		City                 string  `json:"city"`
+		Address1             string  `json:"address_1"`
+		Name                 string  `json:"name"`
+		Lon                  float64 `json:"lon"`
+		ID                   int     `json:"id"`
+		State                string  `json:"state"`
+		Lat                  float64 `json:"lat"`
+		Repinned             bool    `json:"repinned"`
+	} `json:"venue,omitempty"`
+	Headcount      int    `json:"headcount"`
+	Visibility     string `json:"visibility"`
+	WaitlistCount  int    `json:"waitlist_count"`
+	Created        int64  `json:"created"`
+	MaybeRsvpCount int    `json:"maybe_rsvp_count"`
+	Description    string `json:"description"`
+	EventURL       string `json:"event_url"`
+	YesRsvpCount   int    `json:"yes_rsvp_count"`
+	Duration       int    `json:"duration"`
+	Name           string `json:"name"`
+	ID             string `json:"id"`
+	Time           int64  `json:"time"`
+	Updated        int64  `json:"updated"`
+	Group          struct {
+		JoinMode string  `json:"join_mode"`
+		Created  int64   `json:"created"`
+		Name     string  `json:"name"`
+		GroupLon float64 `json:"group_lon"`
+		ID       int     `json:"id"`
+		Urlname  string  `json:"urlname"`
+		GroupLat float64 `json:"group_lat"`
+		Who      string  `json:"who"`
+	} `json:"group"`
+	Status string `json:"status"`
 }
 
-type g struct {
-	join_mode string
-	created   int
-	name      string
-	group_lon float64
-	id        int
-	urlname   string
-	group_lat float64
-	who       string
-}
-
-type res struct {
-	utc_offset       int
-	venue            v
-	headcount        int
-	visibility       string
-	waitlist_count   int
-	created          int
-	maybe_rsvp_count int
-	description      string
-	event_url        string
-	yes_rsvp_count   int
-	duration         int
-	name             string
-	id               string
-	time             int
-	updated          string
-	group            g
-	status           string
-}
-
-type m struct {
-	next        string
-	method      string
-	total_count int
-	link        string
-	count       int
-	description string
-	lon         float64
-	title       string
-	url         string
-	id          string
-	updated     int
-	lat         float64
-}
-
-type rlt struct {
-	result []res
-	meta   m
+type MeetupRes struct {
+	Results []struct {
+		UtcOffset int `json:"utc_offset"`
+		Venue     struct {
+			Zip                  string  `json:"zip"`
+			Country              string  `json:"country"`
+			LocalizedCountryName string  `json:"localized_country_name"`
+			City                 string  `json:"city"`
+			Address1             string  `json:"address_1"`
+			Name                 string  `json:"name"`
+			Lon                  float64 `json:"lon"`
+			ID                   int     `json:"id"`
+			State                string  `json:"state"`
+			Lat                  float64 `json:"lat"`
+			Repinned             bool    `json:"repinned"`
+		} `json:"venue,omitempty"`
+		Headcount      int    `json:"headcount"`
+		Visibility     string `json:"visibility"`
+		WaitlistCount  int    `json:"waitlist_count"`
+		Created        int64  `json:"created"`
+		MaybeRsvpCount int    `json:"maybe_rsvp_count"`
+		Description    string `json:"description"`
+		EventURL       string `json:"event_url"`
+		YesRsvpCount   int    `json:"yes_rsvp_count"`
+		Duration       int    `json:"duration"`
+		Name           string `json:"name"`
+		ID             string `json:"id"`
+		Time           int64  `json:"time"`
+		Updated        int64  `json:"updated"`
+		Group          struct {
+			JoinMode string  `json:"join_mode"`
+			Created  int64   `json:"created"`
+			Name     string  `json:"name"`
+			GroupLon float64 `json:"group_lon"`
+			ID       int     `json:"id"`
+			Urlname  string  `json:"urlname"`
+			GroupLat float64 `json:"group_lat"`
+			Who      string  `json:"who"`
+		} `json:"group"`
+		Status string `json:"status"`
+	} `json:"results"`
+	Meta struct {
+		Next        string `json:"next"`
+		Method      string `json:"method"`
+		TotalCount  int    `json:"total_count"`
+		Link        string `json:"link"`
+		Count       int    `json:"count"`
+		Description string `json:"description"`
+		Lon         string `json:"lon"`
+		Title       string `json:"title"`
+		URL         string `json:"url"`
+		ID          string `json:"id"`
+		Updated     int64  `json:"updated"`
+		Lat         string `json:"lat"`
+	} `json:"meta"`
 }
 
 func main() {
-	response, err := http.Get("https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=WWCWichita&photo-host=public&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=73273692&sig=4111c5adf6695f954bd7ae7dfd86896970b451f6")
+	dat_1 := GetMeetupResults("https://api.meetup.com/2/events?offset=0&format=json&limited_events=False&group_urlname=WWCWichita&photo-host=public&page=20&fields=&order=time&status=upcoming&desc=false&sig_id=73273692&sig=4111c5adf6695f954bd7ae7dfd86896970b451f6")
+	fmt.Println(reflect.TypeOf(dat_1.Results[0]))
+}
+
+func GetMeetupResults(URL string) MeetupRes {
+	var res MeetupRes
+	response, err := http.Get(URL)
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		defer response.Body.Close()
-		var dat rlt
 		body, _ := ioutil.ReadAll(response.Body)
 
-		if err := json.Unmarshal(body, &dat); err != nil {
+		if err := json.Unmarshal(body, &res); err != nil {
 			panic(err)
 		}
-		fmt.Println(dat.result[0])
 	}
+	return res
 }
